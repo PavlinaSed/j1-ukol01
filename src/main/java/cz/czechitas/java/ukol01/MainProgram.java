b57fc53dad110f3d8c7a335e6773eed360b64fcb
Pavlina Sedlackova <p1sedlackova@gmail.com>
2024-03-03 23:12:08 +0100

    Part 2 completed. The sun and the octagon were drawn.

--- src/main/java/cz/czechitas/java/ukol01/MainProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/MainProgram.java
@@ -16,7 +16,17 @@ public class MainProgram {
 
         zofka.setPenColor(Color.decode("100011"));
 
+        newPositionDown();
+        newPositionRight();
+
         drawPiglet();
+
+        newPositionSun();
+        drawSun();
+
+        newPositionRight();
+        newPositionRight();
+        drawOctagon();
     }
 
     public void drawPiglet() {
@@ -76,6 +86,66 @@ public class MainProgram {
         zofka.move(50);
         zofka.turnRight(180);
     }
+
+    public void newPositionRight() {
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.penDown();
+    }
+
+    public void newPositionDown() {
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(400);
+        zofka.turnLeft(180);
+        zofka.penDown();
+    }
+
+    public void newPositionLeft() {
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.penDown();
+    }
+
+    public void newPositionSun() {
+        zofka.penUp();
+        for (int i = 0; i < 4; i++) {
+            this.newPositionLeft();
+        }
+        zofka.penUp();
+        zofka.move(550);
+        zofka.penDown();
+    }
+
+    public void drawSun() {
+        for (int i = 0; i < 18; i++) {
+            zofka.turnRight(20);
+            zofka.move(20); //proportionally increases or decreases the size of the sun
+            drawSunbeam();
+        }
+        zofka.turnLeft(5);
+    }
+
+    public void drawSunbeam() {
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(40);
+        zofka.turnLeft(90);
+    }
+
+public void drawOctagon() {
+    for (int i = 0; i < 8; i++) {
+        zofka.turnRight(45);
+        zofka.move(30);
+    }
+    zofka.turnLeft(5);
+}
+
 
 }
 
